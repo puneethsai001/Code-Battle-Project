e429305051c45a79bbe9da2a3bcaf12d90443da9
Puneeth Sai <puneethsai001@gmail.com>
2024-02-05 15:12:26 +0400

    edited index.php

--- index.php
+++ index.php
@@ -3,29 +3,32 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-    <link rel="stylesheet" href="indexStyle.css">
+    <title>Login - CodeBattle</title>
+    <link rel="stylesheet" href="indexStyle.css"> 
 </head>
-    <body>
-            <div class="container">
-                <div class="form-container"> 
-                    <h1>Welcome to CodeBattle</h1>
-                    <form action="includes/formhandler.inc.php" method="POST">
-                        <p>Please enter your Log in details to proceed.</p>
-                        <label for="username"></label>
-                        <input type="text" id="username" name="username" placeholder="Username" >
-                        <label for="password"></label>
-                        <input type="password" id="password" name="pwd" placeholder="Password" >
-                        
-                        <a href="/">forgot your password?</a>
-                        <hr></hr>
-                        <button type="submit">Log In</button>
-                    </form>
+<body>
+    <div class="container">
+        <div class="form-container">
+            <h1>Welcome Back!</h1>
+            <p>CodeBattle – where young minds transform ideas into digital masterpieces. <br>Join the journey and let your creativity unfold!</p>
+            <form action="includes/formhandler.inc.php" method="POST">
+                <h4>Please enter your login details to proceed.</h4>
+                <div class="form-group">
+                    <label for="username"></label>
+                    <input type="text" id="username" name="username" placeholder="Username">
                 </div>
-                <div class="image-container">
-                        <img src="C:\Users\moham_jvk4ynn\Downloads\hackathonpic.jpeg" alt="Image">
+                <div class="form-group">
+                    <label for="password"></label>
+                    <input type="password" id="password" name="pwd" placeholder="Password">
                 </div>
-            </div>
-    </body>
-</html> 
-
+                <a href="#" class="forgot-password" >Forgot your password?</a>
+                <hr>
+                <button type="submit">Log In</button>
+            </form>
+        </div>
+        <div class="image-container">
+            <img src="Images/hackathonpic.jpeg" alt="CodeBattle Image">
+        </div>
+    </div>
+</body>
+</html>
