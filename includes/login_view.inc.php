740b914154de2bc19ccfb150830022fa7901d1f6
misbaaaah <misba63@gmail.com>
2024-02-07 17:14:53 +0400

    modifications on admin and index for logout

--- includes/login_view.inc.php
+++ includes/login_view.inc.php
@@ -21,16 +21,15 @@ function check_login_errors(){
     }
     else if(isset($_GET["login"])&&$_GET["login"]==="success"){
         header("Location: Admin.html");
-        echo '<h2 style="color: green;">Login success</h2>';
     }
 }
-function output_username(){
-    if(isset($_SESSION["user_id"])){
-        $r=session_id();
-        echo "session id = ".$r."<br>";
-        echo "you are logged in as ".$_SESSION["user_username"]."  ".$_SESSION["user_id"];
-    }
-    else{
-        // echo "you are not logged in";
-    }
-}
+// function output_username(){
+//     if(isset($_SESSION["user_id"])){
+//         $r=session_id();
+//         echo "session id = ".$r."<br>";
+//         echo "you are logged in as ".$_SESSION["user_username"]."  ".$_SESSION["user_id"];
+//     }
+//     else{
+//         // echo "you are not logged in";
+//     }
+// }
